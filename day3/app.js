ef15cefa8a295b6573a5f11468056edbce988974
angdembe_sona <asonaxaviersm1@gmail.com>
2024-02-06 21:51:00 +0545

    MERN_from_dp

---
+++ day3/app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+// to unbox  or to use the install express we have to render here because we need express here [ it is holld using required]
+   const app = express()
+
+// const app =  require('express')   we asssignn the express in app
+
+
+
+app.get("/",(req,res)=>{
+// res.send("Hello world");
+res.send("Hello world");
+})
+
+
+
+
+app.listen(3000,()=>{
+    console.log('Server is running on http://localhost:3000');
+})
+
